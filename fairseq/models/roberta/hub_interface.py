a69c8f01ee18c65060c1be34260d13395b432ceb
Shuo Wang <315665262@qq.com>
2022-02-17 10:16:25 +0800

    Update hub_interface.py

--- fairseq/models/roberta/hub_interface.py
+++ fairseq/models/roberta/hub_interface.py
@@ -119,8 +119,8 @@ class RobertaHubInterface(nn.Module):
         return F.log_softmax(logits, dim=-1)
 
     def predict_siamese(self, tokens0: torch.LongTensor, tokens1: torch.LongTensor):
-        features0 = self.extract_features(tokens0.to(device=self.device))[-1]
-        features1 = self.extract_features(tokens1.to(device=self.device))[-1]
+        features0 = self.extract_features(tokens0.to(device=self.device))
+        features1 = self.extract_features(tokens1.to(device=self.device))
         cos = torch.nn.CosineSimilarity(dim=-1, eps=1e-6)
         cos_sim = cos(features0, features1).item()
         return 1 if cos_sim > 0 else -1
